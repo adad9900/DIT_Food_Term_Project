d5b873f227105387a694acdcfa6554b717853bab
King <king@kings-imac.local>
2018-06-29 01:13:16 +0900

    test2

--- UiTableViewController Test 02/Controller/TotalMapViewController.swift
+++ UiTableViewController Test 02/Controller/TotalMapViewController.swift
@@ -45,9 +45,6 @@ class TotalMapViewController: UIViewController {
                         self.annotations.append(annotation)
                         self.totalMapView.addAnnotations(self.annotations)
         
-                        // Set zoom level
-//                              let region = MKCoordinateRegionMakeWithDistance(annotation.coordinate, 800, 800)
-//                              self.totalMapView.setRegion(region, animated: true)
                     }
                 }
                 self.totalMapView.showAnnotations(self.annotations, animated: true)
@@ -55,14 +52,5 @@ class TotalMapViewController: UIViewController {
         }
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
